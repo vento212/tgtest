fe39b6175a4e259c575ec800b6691aa483c28b46
VENTO <vento@MacBook-Air-VENTO.local>
2025-07-21 06:13:16 +0300

    Simplify app for better stability

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ const manifestUrl = 'https://fancy-melomakarona-ceb24e.netlify.app/tonconnect-ma
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <TonConnectUIProvider manifestUrl={manifestUrl}>
-      <App />
-    </TonConnectUIProvider>
-  </React.StrictMode>
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>
 ); 
